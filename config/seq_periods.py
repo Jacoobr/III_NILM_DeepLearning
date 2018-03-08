7340aa2ae7483f59f81cc3393ae2bb4ff9170cf9
changfangyi <changfangyi12965@gmail.com>
2018-03-08 14:32:00 +0800

    Update seq_periods.py

--- config/seq_periods.py
+++ config/seq_periods.py
@@ -3,9 +3,9 @@
 
 # Sequence periods (in seconds)
 SEQ_PERIODS = {
-    'television': 3600,
-    'air conditioner': 7200,
-    'washing machine': 3600,
-    'fridge': 3600,
-    'bottle warmer': 3600,
+    'television': 360,
+    'air conditioner': 720,
+    'washing machine': 360,
+    'fridge': 360,
+    'bottle warmer': 360,
 }
